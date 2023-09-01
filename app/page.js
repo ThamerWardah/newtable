217bb8431d2e926758506be4121de717a8beaee5
thamer <thamer93work@gmail.com>
2023-09-01 17:32:36 +0300

    version-NewTable-7

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import AddDistributor from "./components/AddDistributor";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 
 export default async function Home(){
-
+  const session = await getServerSession(authOptions)
+  if(session){redirect('/student')}
   return(
     <main className="p-20">
       <section>
