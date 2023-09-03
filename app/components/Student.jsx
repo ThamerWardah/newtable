4ae0201758fce64146d2586903009d89b8251617
thamer <thamer93work@gmail.com>
2023-09-03 18:11:16 +0300

    version-fix-shadow

--- app/components/Student.jsx
+++ app/components/Student.jsx
@@ -48,7 +48,7 @@ const handleUpdate = async()=>{
 
 
     return(
-        <div className="relative h-full w-full">
+        <div className="relative h-full w-full ">
 
                <div className=" flex justify-between shadow-lg font-bold  p-2">
                 <GiHamburgerMenu className="text-2xl" onClick={()=>setHamburger(true)}/>
@@ -61,16 +61,16 @@ const handleUpdate = async()=>{
                  <div className="w-4/5 h-full">
 
                      <div className="w-full h-32  bg-blue-400 flex flex-col py-2 px-4 justify-between  text-white font-bold">
-                        <div ><PiStudentDuotone className="text-6xl shadow-md shadow-blue-800 border-2 border-blue-600 text-black bg-gray-200 rounded-full  p-2"/></div>
+                        <div ><PiStudentDuotone className="text-6xl shadow-md shadow-blue-600 border-2 border-blue-500 text-black bg-gray-200 rounded-full  p-2"/></div>
                       <div className="mb-4">
-                           <h1 className="font-bold text-md ">{user?.name} <span className="text-gray-100 text-lg mx-10">{studentLevel} : <span className="text-gray-200 text-sm">الوحدات المنجزه</span></span></h1>
+                           <h1 className="font-bold text-md text-start ">{user?.name} <span className="text-gray-100 text-lg mx-10">{studentLevel} : <span className="text-gray-200 text-sm">الوحدات المنجزه</span></span></h1>
                                <h1 className="text-gray-300  text-sm">{user?.email}</h1>
                         </div> 
 
                     </div>
 
 
-                        <div className="flex pt-20 h-full flex-col gap-4 bg-gray-100">
+                        <div className="flex pt-20 h-full flex-col gap-4 bg-gray-100 shadow-2xl rounded-br-full">
                         <Link href='/openItemsPage' className="font-bold px-2 m-2">Open Items</Link>
                 {userAdmin?.includes('admin')&&<Link href='/theTable' className="font-bold px-2 m-2">Create Table</Link>}
 
@@ -80,8 +80,18 @@ const handleUpdate = async()=>{
 
                     
                     </div>
+                    <div onClick={()=>setHamburger(false)} className="w-1/5 h-full bg-gradient-to-b from-black/80 to-gray-50 flex justify-center overflow-hidden items-center">
+                        <div className="border-2 border-white/80 w-8 h-8 rounded-full animate-ping">
+                        <div className="border-4 border-blue-400 w-8 h-8 rounded-full animate-ping">
+                        <div className="border-6 border-green-400 w-6 h-6 rounded-full animate-ping">
+                        <div className="border-8 border-yellow-500 w-6 h-6 rounded-full animate-pulse "></div>
+                        </div>
+                        </div>
+                        </div>
+                        
 
-                    <div onClick={()=>setHamburger(false)} className="w-1/5 h-full bg-black/80 "></div>
+                    </div>
+
                </div>}
 
 
