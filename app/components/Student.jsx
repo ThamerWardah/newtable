9ae0951eaf3339569cd785d3846a11fc95112af6
thamer <thamer93work@gmail.com>
2023-09-03 19:44:11 +0300

    version-Icons

--- app/components/Student.jsx
+++ app/components/Student.jsx
@@ -11,6 +11,8 @@ import { useRouter } from "next/navigation";
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {GrLogout} from 'react-icons/gr'
 import{PiStudentDuotone} from 'react-icons/pi'
+import {ImTable} from 'react-icons/im'
+import {LuBookOpenCheck} from 'react-icons/lu'
 
 
 export default function Student({oldFinished ,studentLevel, errorFinished, user}){
@@ -57,30 +59,40 @@ const handleUpdate = async()=>{
                 
             </div>
 
-            {hamburger &&   <div className="absolute  z-50 top-0 left-0 flex w-full h-full">
+            {hamburger &&   <div className="absolute bg-gradient-to-b from-black/70 to-gray-100 z-50 top-0 left-0 flex w-full h-full">
                  <div className="w-4/5 h-full">
 
                      <div className="w-full h-32  bg-blue-400 flex flex-col py-2 px-4 justify-between  text-white font-bold">
                         <div ><PiStudentDuotone className="text-6xl shadow-md shadow-blue-600 border-2 border-blue-500 text-black bg-gray-200 rounded-full  p-2"/></div>
                       <div className="mb-4">
-                           <h1 className="font-bold text-md text-start ">{user?.name} <span className="text-gray-100 text-lg mx-10">{studentLevel} : <span className="text-gray-200 text-sm">الوحدات المنجزه</span></span></h1>
+
+                           <h1 dir="ltr" className="font-bold text-md text-start ">{user?.name}</h1>
+                            <div className="flex justify-between">
                                <h1 className="text-gray-300  text-sm">{user?.email}</h1>
+                               <h1>
+                               <span className="text-gray-100 text-lg mx-10">{studentLevel} : <span className="text-gray-200 text-sm">الوحدات المنجزه</span></span>
+                               </h1>
+                            </div>
                         </div> 
 
                     </div>
 
 
-                        <div className="flex pt-20 h-full flex-col gap-4 bg-gray-100 shadow-2xl rounded-br-full">
-                        <Link href='/openItemsPage' className="font-bold px-2 m-2">Open Items</Link>
-                {userAdmin?.includes('admin')&&<Link href='/theTable' className="font-bold px-2 m-2">Create Table</Link>}
+                        <div className="flex pt-20 h-full flex-col gap-6 px-2 bg-gray-100 shadow-2xl rounded-br-full text-sm font-bold">
 
-                <button onClick={()=>signOut()} className='active:bg-red-500 text-start font-bold  px-2 m-2 ' href='/people'>
-                    <GrLogout className="inline-block text-2xl font-bold pr-2"/>  Sign Out</button>
+                        <Link className=' active:bg-red-500 w-fit' href='/people'><ImTable className="inline-block text-3xl  pr-2"/>الجدول</Link>
+
+                        <Link href='/openItemsPage' className="w-fit"><LuBookOpenCheck className="inline-block text-3xl  pr-2"/> المواد المتاحه</Link>
+
+                {userAdmin?.includes('admin')&&<Link href='/theTable' className="w-fit">Create Table</Link>}
+
+                <button onClick={()=>signOut()} className='active:bg-red-500 text-start w-fit ' href='/people'>
+                <GrLogout className="inline-block text-2xl  pr-2"/> تسجيل خروج</button>
                         </div>
 
                     
                     </div>
-                    <div onClick={()=>setHamburger(false)} className="w-1/5 h-full bg-gradient-to-b from-black/80 to-gray-100 flex justify-center overflow-hidden items-center">
+                    <div onClick={()=>setHamburger(false)} className="w-1/5 h-full bg-transparent flex justify-center overflow-hidden items-center">
                         <div className="border-2 border-white/80 w-8 h-8 rounded-full animate-ping">
                         <div className="border-4 border-blue-400 w-8 h-8 rounded-full animate-ping">
                         <div className="border-6 border-green-400 w-6 h-6 rounded-full animate-ping">
