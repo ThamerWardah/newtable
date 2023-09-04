53bcd37abd75499ce8ba4c92c75bc0380af0da43
thamer <thamer93work@gmail.com>
2023-09-04 20:16:36 +0300

    arabic-table

--- app/components/MainTableCompo.jsx
+++ app/components/MainTableCompo.jsx
@@ -1,16 +1,16 @@
 'use client'
 import {useState} from 'react'
 import Link from 'next/link';
-import { weekDays ,lectureTimeDay } from './data';
+import { weekDays, lectureTimeDay, itemsInArabic } from './data';
 import clsx from 'clsx';
 
 const MainTable = ({onlineT2})=>{
-    const [iFocus,setIFocus]=useState(true);
+    const [iFocus,setIFocus]=useState(false);
     const days = weekDays;
     const lecTime = lectureTimeDay;
     const onlineT = onlineT2?.table
     return(
-        <main className='w-full h-full'>
+        <main dir='rtl' className='w-full h-full '>
 
             <div className="p-4 shadow-sm ">
                 <Link className="px-4 font-bold border-2 mx-4 py-1  shadow-md shadow-blue-500 active:bg-green-400" href='/student'>رجوع</Link>
@@ -22,13 +22,13 @@ const MainTable = ({onlineT2})=>{
                 <div className='w-full flex justify-between border-2 border-black  h-8'>
 
                     <div className='w-1/12 text-center'>
-                        <button className='text-xs font-bold bg-black rounded-md shadow-sm py-1 px-2 text-white' onClick={()=>setIFocus(pre=>!pre)}>تركيز</button>
+                        <button className='text-xs font-bold bg-black rounded-md shadow-sm p-1 text-white' onClick={()=>setIFocus(pre=>!pre)}>تركيز</button>
                     </div>
 
                     <div className='w-11/12 grid grid-cols-6 grid-rows-1 items-center justify-center'>
 
                         {lecTime.map((time,ina)=><div key={ina}
-                            className='text-center text-xs font-bold'>
+                            className='text-center text-[8px] font-bold'>
                             {time}
                         </div>)}
                     </div>
@@ -45,11 +45,11 @@ const MainTable = ({onlineT2})=>{
 
                     <div className={clsx(`w-11/12  border-2 border-black`,iFocus &&'text-gray-300')}>
 
-                        <div className='grid grid-cols-6 grid-rows-5 text-[7px] font-bold'>
+                        <div className='grid grid-cols-6 grid-rows-5 text-[9px] font-bold'>
                             { onlineT.map((hour,index)=><div key={index} className='border-[1px] border-black py-2 px-1 bg-white' >
 
                                     {hour.map((item,i)=><div key={i+100} className='flex flex-col gap-1'>
-                                        {item}
+                                        {itemsInArabic[item.slice(0,1)]}{item.slice(1)}
                                     </div>)}
 
                             </div>) }
@@ -64,9 +64,6 @@ const MainTable = ({onlineT2})=>{
               
 
             </div>
-
-
-   
 
 
         </main>
