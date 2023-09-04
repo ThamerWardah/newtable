c514506673928231c9543ea44d79fef838e7ac67
thamer <thamer93work@gmail.com>
2023-09-05 00:45:11 +0300

    new

--- app/(table)/mainTable/page.jsx
+++ app/(table)/mainTable/page.jsx
@@ -1,9 +1,18 @@
 import onelineTable from "../../actions/onlineTable";
-import MainTable from '../../components/MainTableCompo'
+import MainTable from '../../components/MainTableCompo';
+import prisma from '../../Libs/prismad'
 
 
 export default async function MainTableFunction(){
-    const onlineT2 = await onelineTable() ;
+
+    const table = await prisma.table.findUnique({
+        where:{
+            id:'64f636d0550fee5b24eeca6e'
+        }
+    });
+
+   /*  const onlineT2 = await onelineTable() ; */
+   const onlineT2 = table
     return(
 
         <>
