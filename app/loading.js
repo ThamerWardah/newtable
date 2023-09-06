0148046c6ae5d6d458773496646d1c87fdb67a8e
thamer <thamer93work@gmail.com>
2023-09-06 22:43:30 +0300

    version Loaing Wecome

--- app/loading.js
+++ app/loading.js
@@ -6,7 +6,10 @@ export default function LoaingStudent(){
      
         <div className="p-1 rounded-md block relative overflow-hidden  ">
              <div className="z-0 absolute animate-spin bottom-[-25%] left-[-68%]  w-[220%] h-[150%] bg-gradient-to-tr from-red-500 to-green-500"></div>
-             <div className="text-blue-600 relative bg-gray-100 p-1 z-50 text-4xl font-bold text-center"> Loading</div>
+             
+             <div className="w-full h-full rounded-md relative bg-gray-100 z-50 py-6 px-4">
+             <div className="text-green-600 text-6xl font-bold text-center bg-gray-100"> Welcome</div>
+             </div>
          </div>
    
        </main>
